eafbd18f1244e7772c70e0eca43aa3872717fc07
Md.Raihanul Islam Rafi <19-41790-3@student.aiub.edu>
2023-02-18 02:29:52 +0600

    Full Done

--- DatabaseCRUD/Controllers/ProductController.cs
+++ DatabaseCRUD/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DatabaseCRUD.EF;
+using DatabaseCRUD.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,61 @@ namespace DatabaseCRUD.Controllers
     public class ProductController : Controller
     {
         // GET: Product
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Product model)
+        {
+            //if (ModelState.IsValid) { }
+            //dbinsert
+            var db = new Test02Entities();
+            db.Products.Add(model);
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+        public ActionResult List()
+        {
+            var db = new Test02Entities();
+            var list = db.Products.ToList();
+            return View(list);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var db = new Test02Entities();
+            var ed = (from s in db.Products
+                           where s.Id == id
+                           select s).SingleOrDefault();
+            return View(ed);
+        }
+        [HttpPost]
+        public ActionResult Edit(Product updated)
+        {
+            var db = new Test02Entities();
+            var upd = (from s in db.Products
+                              where s.Id == updated.Id
+                              select s).SingleOrDefault();
+            //upd.Name = updated.Name;
+            //upd.Price = updated.Price;
+            //upd.Qty = updated.Qty;
+            db.Entry(upd).CurrentValues.SetValues(updated);
+            db.SaveChanges();
+            return RedirectToAction("List");
+            
+        }
+        
+        public ActionResult Delete(Product updated)
+        {
+            var db = new Test02Entities();
+            var upd = (from s in db.Products
+                       where s.Id == updated.Id
+                       select s).SingleOrDefault();
+            db.Products.Remove(upd);
+            db.SaveChanges();
+            return RedirectToAction("List");
         }
     }
 }
